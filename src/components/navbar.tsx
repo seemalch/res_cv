9eaa14acb0c116204e27268e6329bb9843dfd252
Seemal Zia <seemalch5@gmail.com>
2025-04-28 05:54:26 +0500

    .

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react"
 import Link from "next/link"
 import { Menu, X } from 'lucide-react'
 
+
 const navLinks = [
   { name: "Home", href: "#home" },
   { name: "About", href: "#about" },
